1129ab9b81c508a085d8f4105051973bcb1f4e08
DavelDs <163164280+DavelDs@users.noreply.github.com>
2025-12-01 16:57:18 +0700

    Commit No. 3 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.test_lab_week_12
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.test_lab_week_12.model.Movie
@@ -9,30 +8,40 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.catch
+import java.util.Calendar
 
 class MovieViewModel(private val movieRepository: MovieRepository)
     : ViewModel() {
+
     init {
         fetchPopularMovies()
     }
 
-    // define the LiveData
-    private val _popularMovies = MutableStateFlow(
-        emptyList<Movie>()
-    )
+    private val _popularMovies = MutableStateFlow(emptyList<Movie>())
     val popularMovies: StateFlow<List<Movie>> = _popularMovies
+
     private val _error = MutableStateFlow("")
     val error: StateFlow<String> = _error
 
-    // fetch movies from the API
     private fun fetchPopularMovies() {
         viewModelScope.launch(Dispatchers.IO) {
-            movieRepository.fetchMovies().catch {
-                _error.value = "An exception occurred: ${it.message}"
-            }.collect {
-                _popularMovies.value = it
-            }
-        }
 
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR).toString()
+
+            movieRepository.fetchMovies()
+                .catch {
+                    _error.value = "An exception occurred: ${it.message}"
+                }
+                .collect { moviesList ->
+
+                    val filtered = moviesList
+                        .filter { movie ->
+                            movie.releaseDate?.startsWith(currentYear) == true
+                        }
+                        .sortedByDescending { it.popularity }
+
+                    _popularMovies.value = filtered
+                }
+        }
     }
 }
